d376adb55dd857b2b72049b6e73aedacda793c8d
Tore Aurstad <tore.aurstad@hemit.no>
2022-01-29 00:09:04 +0100

    Module 5

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -18,6 +18,7 @@ namespace BugTrackerUI
         [MinLength(10)]
         public string Description { get; set; }
 
+        [Required]
         [Range(1, 5, ErrorMessage = "Oppgi et tall 1-5 silly")]
         public int Priority { get; set; }
     }
